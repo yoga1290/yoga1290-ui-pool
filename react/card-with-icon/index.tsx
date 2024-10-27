9fc0ed993f0de7717cbfe51fe8938016dc609959
Youssef Gamil <yoga1290@gmail.com>
2024-10-26 20:02:35 -0700

    style changes

--- react/card-with-icon/index.tsx
+++ react/card-with-icon/index.tsx
@@ -14,9 +14,9 @@ const doNothing: React.MouseEventHandler<HTMLDivElement> = () => {};
 export default ({ title, text, subtitle, click, icon }: CardProps) => (
         <div className={`card-with-icon animate__animated animate__fadeInUp col-12 align-self-stretch d-flex text-center`}>
 
-            <div className="card bg-dark col-12 border-light pointer-cursor py-2 my-4" tabIndex={0} onClick={!!click? click:doNothing}>
+            <div className="card bg-dark col-12 border-light pointer-cursor py-2 my-2" tabIndex={0} onClick={!!click? click:doNothing}>
             <div className="card-body">
-                <span className="material-symbols-outlined align-middle p-5 fs-1">{ icon }</span>
+                <span className="material-symbols-outlined align-middle my-4 fs-1">{ icon }</span>
                 <h5 className="card-title user-select-all">{ title }</h5>
                 <p className={`card-subtitle user-select-all mb-2 text-muted ${subtitle ? '':'invisible'}`}>
                     { subtitle }
