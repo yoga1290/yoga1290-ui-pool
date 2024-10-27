aba20ddad48986feee140de632bb88244d4e842f
Youssef Gamil <yoga1290@gmail.com>
2024-10-26 20:02:35 -0700

    style changes

--- react/card-featured-with-buttons/index.tsx
+++ react/card-featured-with-buttons/index.tsx
@@ -26,7 +26,7 @@ const getCSSBackgroud = (backgroundImageUrl: string = '') => ({
 export default ({ title, text, subtitle, backgroundImageUrl, buttons }: CardProps) => (
         <div className={`card-featured-with-buttons animate__animated animate__fadeInUp col-12 align-self-stretch d-flex`}>
 
-            <div className="card bg-dark col-12 border-light pointer-cursor py-2 my-4" tabIndex={0}
+            <div className="card bg-dark col-12 border-light pointer-cursor py-2 my-2" tabIndex={0}
                 style={getCSSBackgroud(backgroundImageUrl)}>
             <div className="card-body card-featured__body">
 
