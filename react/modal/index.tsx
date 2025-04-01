269da613b3c90b33d14bf90f5c1980589e7a7640
Youssef Gamil <yoga1290@gmail.com>
2025-04-01 14:28:12 -0700

    modal

--- react/modal/index.tsx
+++ react/modal/index.tsx
@@ -11,29 +11,27 @@ export type ModalProp = {
 
 export default ( {content, show, onClose }: ModalProp) => {
 
-    const [visible, setVisible] = useState<boolean>(show);
-    // const setVisible = (flag: boolean) => {
-    //     console.log('setVisible', flag);
-    //     if (!!onClose && !flag) {
-    //         onClose(flag);
-    //     }
-    //     // _setVisible(flag);
-    // };
-
+    const [visible, _setVisible] = useState<boolean>(show);
+    const setVisible = (flag: boolean) => {
+        if(!!onClose && !!!flag) {
+            onClose();
+        }
+        _setVisible(flag);
+    };
     useEffect(()=> (setVisible(show)), [show]);
 
     return (
-    <div className={`modal-page ${visible? '':'d-none'}`}>
+    <div className={`modal-page ${!!visible? '':'d-none'}`}>
 
-        <div className={`modal-page__background animate__animated animate__faster ${visible? 'animate__fadeIn':'d-none'}`}
-            onClick={() => (!!onClose && onClose(false))}></div>
+        <div className='modal-page__background animate__animated animate__fadeIn'
+            onClick={() => (setVisible(false))}></div>
 
         <div className='modal-page__modal'>
             
-            <div className={`modal-page__modal-content`}
+            <div className='modal-page__modal-content'
                 onClick={(e) => (e.preventDefault())}>
                 
-                <div className={`row justify-content-center px-0 animate__animated animate__fast ${visible? 'animate__fadeInUp':'animate__fadeOutUp'}`}>
+                <div className='row justify-content-center px-0 animate__animated animate__fadeInUp animate__delay-1s'>
 
                     {content}
                     
